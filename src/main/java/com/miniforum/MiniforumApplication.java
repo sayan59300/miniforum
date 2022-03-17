c172c2cfbcaf75f68294f2ba9199242529f1202a
Buffart Nicolas <nicolas.buffart@itval.fr>
2022-03-17 15:30:29 +0100

    first commit

---
+++ src/main/java/com/miniforum/MiniforumApplication.java
@@ -0,0 +1,13 @@
+package com.miniforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniforumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniforumApplication.class, args);
+    }
+
+}
