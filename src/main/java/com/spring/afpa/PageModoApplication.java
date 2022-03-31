c4d67e5147c455a6847f27504553664f840e8ece
BlackEyes59 <cadart.antoine59@gmail.com>
2022-03-31 09:47:33 +0200

    Création de la page modérateur

---
+++ src/main/java/com/spring/afpa/PageModoApplication.java
@@ -0,0 +1,13 @@
+package com.spring.afpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PageModoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PageModoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/afpa/PageModoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.afpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PageModoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
